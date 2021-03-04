199674bbc5951ce239b0bc40ec5ab38361e8a936
Zach <zdb1994@yahoo.com>
2021-03-03 21:09:03 -0500

    Finished chapter 13.

---
+++ Chapter13/exercise.fsx
@@ -0,0 +1,17 @@
+﻿open System
+open System.IO
+
+type Customer = { Name: string; Age: int; }
+
+let printCustomerAge writer customer =
+    if customer.Age >= 18 then writer "Adult!"
+    elif customer.Age >= 13 then writer "Teenager!"
+    else writer "Child!"
+
+let writeToFile text = File.WriteAllText(@"C:/Dev/test/chapter13exercise.txt", text)
+   
+// Partially applied, constrained function for writing to the console.
+let printToConsole = printCustomerAge Console.WriteLine
+
+// Partially applied, constrained function for writing to a file.
+let printToFile = printCustomerAge writeToFile
---
+++ Chapter13/higherorderfuncs1.fsx
@@ -0,0 +1,13 @@
+﻿type Customer = { Age: int }
+
+let where filter customers =
+    seq {
+        for customer in customers do
+            if filter customer then
+                yield customer }
+
+let customers = [ { Age = 21 }; { Age = 30 }; { Age = 26 } ]
+let isOver25 customer = customer.Age > 25
+
+customers |> where isOver25
+customers |> where (fun customer -> customer.Age > 25)
